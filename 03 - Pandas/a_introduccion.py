0584c271da4272f646d7bcdb1d93205219e0dace
Adrian Eguez <eadepto@hotmail.com>
2020-07-14 08:58:26 -0500

    pandas series

---
+++ 03 - Pandas/a_introduccion.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 14 07:41:10 2020
+
+@author: dev-11
+"""
+
+print("Hola spider")
+
+nombre = "Adrian"
+numero = 3
+lista = [1,2,3]
+tupla = (4,5,6)
+diccionario = {
+    "nombre": "Adrian",
+    "apellido": "Eguez"
+    }
+
