6a1cc01ac123bba77f7cef85248be98b48e829f3
Adrian Eguez <eadepto@hotmail.com>
2020-07-25 10:59:11 -0500

    dataframes

--- 03 - Pandas/c_dataframes.py
+++ 03 - Pandas/c_dataframes.py
@@ -11,3 +11,69 @@ Created on Tue Jul 21 08:46:44 2020
 import numpy as np
 import pandas as pd
 
+arr_pand = np.random.randint(0,10,6).reshape(2,3)
+
+df1 = pd.DataFrame(arr_pand)
+
+s1 = df1[0]
+
+# operacion con la serie
+
+s2 = df1[1]
+
+s3 = df1[2]
+
+df1[3] = s1
+
+df1[4] = s1 * s2
+
+
+datos_fisicos_uno = pd.DataFrame(
+    arr_pand,
+    columns = [
+        'Estatura (cm)',
+        'Peso (kg)',
+        'Edad (anios)'])
+
+datos_fisicos_dos = pd.DataFrame(
+    arr_pand,
+    columns = [
+        'Estatura (cm)',
+        'Peso (kg)',
+        'Edad (anios)'],
+    index = [
+        'Adrian',
+        'Vicente'])
+
+serie_peso = datos_fisicos_dos['Peso (kg)']
+datos_adrian = serie_peso['Adrian']
+print(serie_peso)
+print(datos_adrian)
+
+
+df1.index = ['Adrian', 'Vicente']
+df1.index = ['Wendy', 'Carolina']
+df1.columns = ['A', 'B', 'C', 'D', 'E']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
