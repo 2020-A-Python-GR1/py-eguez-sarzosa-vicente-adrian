b95df8ec614cd5c258702d1a8f23d95ed061b657
Adrian Eguez <eadepto@hotmail.com>
2020-08-11 08:53:01 -0500

    iloc avanzado

--- 03 - Pandas/g_iloc_loc.py
+++ 03 - Pandas/g_iloc_loc.py
@@ -31,6 +31,7 @@ print(serie_vertical.index) # Indices son los Indices
 df_1035 = df[df.index == 1035]
 
 # loc -> acceder grupo filas y columnas x LABEL (ARR TRUE FALSE)
+
 segundo = df.loc[1035]  # Filtrar por indice (1)
 segundo = df.loc[[1035,1036]] # Filtrar por arr indices
 
@@ -39,11 +40,38 @@ segundo = df.loc[3:5] # Filtrando desde x indice
 segundo = df.loc[df.index == 1035] # Filtrar por 
                                    # Arreglo-> True False
 
+
 segundo = df.loc[1035, 'artist'] # 1 Indice
 segundo = df.loc[1035, ['artist', 'medium']] # Varios indices
+segundo = df.loc[1035, ['artist', 'medium']]
 
-# print(df.loc[0]) # Indice dentro del DataFrame
-# print(df[0]) # Indice dentro del DataFrame
+# print(df.loc[0]) # Indice dentro del DataFrame X ERROR
+# print(df[0]) # Indice dentro del DataFrame X ERROR
+
+# iloc -> acceder grupo filas y columnas indices en 0
+
+tercero = df.iloc[0]
+tercero = df.iloc[[0,1]]
+tercero = df.iloc[0:10]
+tercero = df.iloc[df.index == 1035]
+
+
+tercero = df.iloc[0:10, 0:4] # Filtrado indices
+                             # Por rango de indice 0:4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
